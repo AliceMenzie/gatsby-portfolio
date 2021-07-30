2b944232d59b97d665b5ea9c7bd73f45f4770331
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-30 22:14:48 +1000

    updating home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
-import Layout from "./components/Layout"
-
+import Layout from "../components/Layout"
+import HeroHome from "../components/HeroHome"
+import TransitionButton from "../components/TransitionButton"
 export default function Home() {
   return (
     <Layout>
   <section>    
 <div>
-<h1>Hello World,</h1>
-<h2>i'm Alice.</h2>
+<h1 className="logo">Hello World,</h1>
+<HeroHome />
+<TransitionButton />
 </div>
   </section>
   </Layout>
