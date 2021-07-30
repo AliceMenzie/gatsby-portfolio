54983b50d1da42ae5cde354892824c981a73a6d6
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-30 22:13:41 +1000

    transition button for home page

---
+++ src/components/TransitionButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/global.css"
+export default function TransitionButton() {
+    return (
+        <div >
+            <svg className="button" width="102" height="82" viewBox="0 0 102 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line x1="20.3104" y1="26.071" x2="53.1293" y2="58.9333" stroke="#EDE9E1" stroke-width="4" />
+                <line x1="50.485" y1="58.8816" x2="83.485" y2="26.0661" stroke="#EDE9E1" stroke-width="4" />
+            </svg>
+        </div>
+    )
+}
