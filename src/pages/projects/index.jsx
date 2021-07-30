585a5861a0a50fda0d40367fa940cca8765b9ab4
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-30 22:12:40 +1000

    added idnex page for projects

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,15 +1,58 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
-import Layout from "../components/Layout"
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
+import { GatsbyImage} from "gatsby-plugin-image"
 
-
-export default function Projects() {
+export default function Projects({ data }) {
+    console.log(data)
+    const projects = data.allMarkdownRemark.nodes
+    // const image = getImage(data.allMarkdownRemark.nodes.frontmatter.thumb)
     return (
         <div>
             <Layout>
-                <h1>Projects</h1>
+                <div>
+                    <h1 className={styles.hero}>Projects</h1>
+                </div>
+                <div className={styles.projects}>
+
+                    {projects.map(project => (
+                        <Link className={styles.project}  to={"/projects/" + project.frontmatter.slug} key={project.id}>
+                            <div>
+                                {/* <GatsbyImage img={project.frontmatter.thumb.childImageSharp.gatsbyImageData} alt="not working"/> */}
+                                <h2>{project.frontmatter.title}</h2>
+                                <p>{project.frontmatter.stack}</p>
+                                <p>{project.frontmatter.deployment}</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </Layout>
 
 
         </div>
     )
 }
+
+// export query project metadata
+export const query = graphql`
+query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          deployment
+          slug
+          thumb {
+            childrenImageSharp {
+              gatsbyImageData(width: 200, placeholder: BLURRED)
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+  
+`
