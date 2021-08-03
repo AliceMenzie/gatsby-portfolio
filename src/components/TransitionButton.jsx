05c99466ecee6f51a6c468812d0165340e01e9b1
Alice Menzie <Alice.Menzie@gmail.com>
2021-08-03 15:10:43 +1000

    changed transition button

--- src/components/TransitionButton.jsx
+++ src/components/TransitionButton.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import "../styles/global.css"
+import { BsChevronBarDown } from "@react-icons/all-files/bs/BsChevronBarDown";
+import { BsChevronDown } from "@react-icons/all-files/bs/BsChevronDown";
 export default function TransitionButton() {
     return (
-        <div >
-            <svg className="button" width="102" height="82" viewBox="0 0 102 82" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line x1="20.3104" y1="26.071" x2="53.1293" y2="58.9333" stroke="#EDE9E1" stroke-width="4" />
-                <line x1="50.485" y1="58.8816" x2="83.485" y2="26.0661" stroke="#EDE9E1" stroke-width="4" />
-            </svg>
+        <div className="wrapper">
+            <div className="button" >
+                {/* < BsChevronBarDown size={50} /> */}
+                < BsChevronDown size={50} />
+            </div>
+
         </div>
+
     )
 }
