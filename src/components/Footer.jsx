0530d0a17a114b43872ef5cc838406b294243acc
AliceMenzie <alice.menzie@gmail.com>
2021-09-24 16:39:02 +1000

    feat: change footer content and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import * as styles from "../styles/footer.module.css"
-// import { DiRuby } from "@react-icons/all-files/di/DiRuby";
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 
 export default function Footer() {
     return (
         <footer >
-            <div className={styles.contact}><p>Contact Me</p></div>
             <div className={styles.links}>
-                <div className={styles.link}><a href="https://www.linkedin.com/in/alice-menzie/" target="_blank" rel="noreferrer"><FaLinkedin size={30} /></a> </div>
+                <div ><a className={styles.link} href="https://www.linkedin.com/in/alice-menzie/" target="_blank" rel="noreferrer"><FaLinkedin size={30} /></a> </div>
                 <div ><a className={styles.link} href="https://github.com/AliceMenzie" target="_blank" rel="noreferrer"><FaGithub size={30} /></a></div>
+                <div ><a className={styles.link} href="https://www.frontendmentor.io/profile/AliceMenzie" target="_blank" rel="noreferrer">Frontend <span>Mentor</span></a></div>
             </div>
         </footer>
     )
